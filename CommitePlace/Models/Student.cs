3750efbf3f61fc7756a948a057f0876ee3ef268d
ayonis <58041529+ayonis@users.noreply.github.com>
2024-04-28 11:20:20 +0300

    Update Student.cs

--- CommitePlace/Models/Student.cs
+++ CommitePlace/Models/Student.cs
@@ -1,11 +1,15 @@
-﻿namespace CommitePlace.Models
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Commiteeplace.Models
 {
     public class Student
     {
-        public string Identification { get; set; }
+        [Key]
+        public string Id { get; set; }
         public string ArabicName { get; set; }
         public string Degree { get; set; }
         public string Center { get; set; }
-        public ICollection<StudentCourse> StudentCourses { get; set; }
+        public List<StudentCourse> StudentCourses { get; set; }
     }
 }
