ee4c3fa37ec9c8854cf99288f46bada2733aed60
ayonis <58041529+ayonis@users.noreply.github.com>
2024-04-28 11:21:48 +0300

    Update CommitDB.cs

--- CommitePlace/Models/CommitDB.cs
+++ CommitePlace/Models/CommitDB.cs
@@ -1,33 +1,16 @@
-﻿using CommitePlace.Models;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
+using Microsoft.Extensions.Options;
 
-namespace CommitePlace.Models
+namespace Commiteeplace.Models
 {
     public class CommitDB : DbContext
     {
-        public CommitDB(DbContextOptions<CommitDB> options) : base(options)
+        protected override void OnConfiguring (DbContextOptionsBuilder options)
         {
+            options.UseSqlServer(@"Data Source=DESKTOP-TGSO878;Initial Catalog=CommitDB;Integrated Security=True;TrustServerCertificate=True");
         }
-
         public DbSet<Student> Students { get; set; }
         public DbSet<Course> Courses { get; set; }
         public DbSet<StudentCourse> StudentCourses { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<StudentCourse>()
-                .HasKey(sc => new { sc.StudentId, sc.CourseId });
-
-            modelBuilder.Entity<StudentCourse>()
-                .HasOne(sc => sc.Student)
-                .WithMany(s => s.StudentCourses)
-                .HasForeignKey(sc => sc.StudentId);
-
-            modelBuilder.Entity<StudentCourse>()
-                .HasOne(sc => sc.Course)
-                .WithMany(c => c.StudentCourses)
-                .HasForeignKey(sc => sc.CourseId);
-        }
     }
 }
