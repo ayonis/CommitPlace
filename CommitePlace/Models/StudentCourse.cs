0d054ca5457be42c4e447ccc776a81ade5d27901
ayonis <58041529+ayonis@users.noreply.github.com>
2024-04-28 11:49:05 +0300

    Update StudentCourse.cs

--- CommitePlace/Models/StudentCourse.cs
+++ CommitePlace/Models/StudentCourse.cs
@@ -6,15 +6,14 @@ namespace Commiteeplace.Models
 {
     [PrimaryKey(nameof(StudentId), nameof(CourseId))]
     public class StudentCourse
-    {
-        
+    {       
         [ForeignKey("Student")]
         public string StudentId { get; set; }
         [ForeignKey("Course")]
        
         public string CourseId { get; set; }
        
-        public List<Student> Student { get; set; }
-        public List<Course> Course { get; set; }
+        public Student Student { get; set; }
+        public Course Course { get; set; }
     }
 }
