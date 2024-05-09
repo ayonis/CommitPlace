dcdb52bdefeb39974ad23dd6f8b3209ec5eff3be
ayonis <58041529+ayonis@users.noreply.github.com>
2024-05-09 22:23:47 +0300

    Add files via upload

---
+++ CommittePlace/ViewModel/StudentDataViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace CommittePlace.ViewModel
+{
+    public class StudentDataViewModel
+    {
+        public string StudentId { get; set; }
+        public string StudentArabicName { get; set; }
+        public string StudentDegree { get; set; }
+        public string StudentCenter { get; set; }
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string ExamTime { get; set; }
+        public string RoomId { get; set; }
+        public string RoomPlace { get; set; }
+    }
+}
